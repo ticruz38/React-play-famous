ea102420958815b5e2fa73bebcdb37d5b231cb52
Ticruz <thib.duchene@gmail.com>
2015-03-21 15:29:52 +0100

    1st integration => View.prototype.commit is called

--- views/react.jsx
+++ views/react.jsx
@@ -1,5 +1,10 @@
 var React = require('react/addons');
 var data = require('../data/food.json');
+var View = require('../helpers/view');
+
+var Engine = require('famous/core/Engine');
+
+var Context = Engine.createContext();
 
 var IndexPage = React.createClass({
 
@@ -10,7 +15,10 @@ var IndexPage = React.createClass({
     },
 
     componentDidMount: function () {
-
+        this.famous = new View({
+            element : this.getDOMNode()
+        });
+        Context.add(this.famous);
     },
 
     onClickLogged: function () {
@@ -22,35 +30,41 @@ var IndexPage = React.createClass({
     render: function () {
         var logged = logged ? 'Log Out' : 'Log In';
         return ( < div >
-            <h1> Ambrosia < /h1> <div> {logged} </div> < div className = 'container' > {
-                this.props.food.entities.map(function (type) {
-                    var child = <FoodEntity data = {type} />
-                    return React.addons.createFragment({a: <div/> , b: child});
-                })
-            } </div>
+            <h1>Ambrosia</h1>
+            <div> {logged} </div>
+                <FoodEntityList>
+                {this.props.datas.map(function (data) {
+                    return <FoodEntity type = {data.type} />
+                })}
+                </FoodEntityList>
             </div>
         );
     }
 });
 
+var FoodEntityList =React.createClass({
+    render: function () {
+    return (
+        <ul>
+        {this.props.children}
+        </ul>
+        );
+    }
+})
+
 var FoodEntity = React.createClass({
     render: function () {
-        console.log(this.props);
         return (
         <div className = 'foodEntity-container'>
-            <h1> Yepoo </h1> <img className = 'foodEntity-image' />
+            <h1> {this.props.type} </h1>
+            <img className = 'foodEntity-image' />
         </div>
         );
     }
 });
 
-var Index = React.render(
-        <IndexPage food = {data[0]}/>,
-        document.body
-        );
+React.render(
+        <IndexPage datas = {data}/>,
+        document.querySelector('.famous-container')
+);
 
-React.Children.count(Index);
-/*
-React.Children.map(Index, function(child){
-});
-*/
